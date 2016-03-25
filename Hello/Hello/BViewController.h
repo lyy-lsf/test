285438293754d3b1801e8c21b1de7ffca552031f
hu <hu17703378010@163.com>
2016-03-25 15:13:29 +0800

    123

---
+++ Hello/Hello/BViewController.h
@@ -0,0 +1,13 @@
+//
+//  BViewController.h
+//  Hello
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BViewController : UIViewController
+
+@end
